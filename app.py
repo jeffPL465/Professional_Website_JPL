95a63bb03e70a45386b2cf16657458f46e33f318
Jeffrey <japoncelopez@wpi.edu>
2026-07-22 19:21:20 -0400

    test columns and resizing

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
-import PIL
+from PIL import Image
 import pathlib
 import os
 
-st.title("Jeffrey Ponce Lopes")
-st.title("Professional Portfolio")
-st.image('Pics_Vids/test_image1.png')
+col1, col2 = st.columns(2)
+
+with col1:
+    st.title("Jeffrey Ponce Lopes")
+    st.title("Professional Portfolio")
+
+with col2: 
+    st.image('Pics_Vids/test_image1.png', width=400)
+
 
 # Add sidebar with buttons for each area
 with st.sidebar:
