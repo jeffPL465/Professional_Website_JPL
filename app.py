c34c8f9abfd0fcc70a1b340970064ab716a0cc98
Jeffrey <japoncelopez@wpi.edu>
2026-07-22 19:02:10 -0400

    add sidebar test

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import pathlib
 import os
 
 st.title("El Jefe's Professional Portfolio")
+
+# Add sidebar with buttons for each area
+with st.sidebar():
+    st.title("Welcome Chuds")
+    st.text("This shit is wild")
+
+    st.button('touch me =)')
+    
